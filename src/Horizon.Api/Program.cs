613399c38b667440082e8d8ea75ccc6cb25fb8a6
Kamil <Str4ngerGameOne@gmail.com>
2025-01-17 01:26:10 +0100

    feat: Add Dapper, mssql connection & layer extension methods.

--- src/Horizon.Api/Program.cs
+++ src/Horizon.Api/Program.cs
@@ -1,4 +1,12 @@
+using Horizon.Application;
+using Horizon.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddApplication()
+    .AddInfrastructure(builder.Configuration);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
---
+++ src/Horizon.Infrastructure/Persistence/ConnectionFactory.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Horizon.Infrastructure.Persistence;
+
+internal sealed class ConnectionFactory(string connectionString) : IConnectionFactory
+{
+    public SqlConnection Create() => new(connectionString);
+}
